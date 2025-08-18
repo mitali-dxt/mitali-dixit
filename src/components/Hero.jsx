57bf531d34b305e490c1d4dc0e193757459c97a6
Mitali Dixit <131600078+mitali-dxt@users.noreply.github.com>
2025-08-18 23:30:36 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,8 +19,7 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Mitali</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I am an upcoming SWE intern at Google <br className='sm:block hidden' />
-            and I absolutely love problem solving.
+            I absolutely love problem solving.
           </p>
         </div>
        
